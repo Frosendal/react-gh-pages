ec36e682705cd62d729de33c3dc587ab2687ac66
Frosendal <Frederik.rosendal@hotmail.com>
2020-03-02 08:56:03 +0100

    Updates

---
+++ precache-manifest.72738b1b88f676e591b3ac6b054c5d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993c34f2e2e623bc8272703d5ff2bb98",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "33b301f4ef19060d3926",
+    "url": "/react-gh-pages/static/css/main.c750d992.chunk.css"
+  },
+  {
+    "revision": "3b867ec66d74b94cd12b",
+    "url": "/react-gh-pages/static/js/2.00e8cbbb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.00e8cbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b301f4ef19060d3926",
+    "url": "/react-gh-pages/static/js/main.ea55fa39.chunk.js"
+  },
+  {
+    "revision": "54001be62eebd59a3712",
+    "url": "/react-gh-pages/static/js/runtime-main.8f268fec.js"
+  }
+]);
